f9000289062c05cd9160090148273bbe47effc91
Ramesh Prajapati <ramesh.prajapati@bacancytechnology.com>
2020-01-10 10:00:42 +0530

    removed unnecessary package and code cleaning

--- src/app/Services/export.service.ts
+++ src/app/Services/export.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import * as FileSaver from 'file-saver';
-import * as XLSX from 'xlsx';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +8,56 @@ export class ExportService {
 
   constructor() { }
 
-  fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-  fileExtension = '.csv';
-
-  public exportExcel(jsonData: any[], fileName: string): void {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
-    const wb: XLSX.WorkBook = { Sheets: { 'data': ws }, SheetNames: ['data'] };
-    const excelBuffer: any = XLSX.write(wb, { bookType: 'csv', type: 'array' });
-    this.saveExcelFile(excelBuffer, fileName);
+  exportExcel(jsonData, fileName) {
+    this.downloadFile(this.generateCSV(jsonData), this.createFilename(fileName));
   }
 
-  private saveExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], {type: this.fileType});
-    FileSaver.saveAs(data, fileName + this.fileExtension);
-  }  
+  downloadFile(data: any, filename: string) {
+    const blob = new Blob([data], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
+  generateCSV(jsonData): any {
+    const columns = ['Name', 'Designation', 'Number', 'Email'];
+    const titles = ['Name', 'Designation', 'Number', 'Email'];
+    let csv = this.toCSV(jsonData, columns, titles);
+    return csv;
+  }
+
+  createFilename(productionDate): string {
+    let filename = productionDate;
+    filename += '.csv';
+
+    return filename;
+  }
+
+  toCSV(items, columns, header = null) {
+    const replacer = (key, value) => value === null || value == undefined ? '' : value;
+
+    if (!columns) {
+      columns = Object.keys(items[0]);
+    }
+
+    let csv = items.map(
+      row => columns.map(
+        fieldName => JSON.stringify(row[fieldName], replacer),
+      ).join(','));
+
+    if (!header) {
+      header = columns;
+    }
+
+    csv.unshift(header.join(','));
+    csv = csv.join('\r\n');
+
+    return csv;
+  }
 }
